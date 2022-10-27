f281277e37761cc33b87d3d80788ec112d982cc9
Michael Fenn <mgfenn157@gmail.com>
2022-10-27 13:59:38 -0400

    changed location to UNCC

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
+    const UNCC = { lat: 35.30910662583227, long: -80.73357613232336 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
@@ -9,7 +9,7 @@ function initMap() {
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: UNCC,
       map: map,
     });
   }
