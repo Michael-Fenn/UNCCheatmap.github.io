78f8f7347234d8364303ca5249cb2dfc90530198
Michael Fenn <mgfenn157@gmail.com>
2022-11-17 16:09:29 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -18,7 +18,7 @@ map.on('load', () => {
     type:'geojson',
     data:'https://mikesmind.blog/UNCCheatmap.github.io/data.geojson'
   })
-  map.setPaintProperty(1060166212,'fill-color', '#ff0000')
+  map.setPaintProperty('building','fill-color', '#ff0000')
 });
 /*map.on("load", function() {
 
