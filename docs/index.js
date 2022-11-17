22b7d5f0f81471de09c7498c35c2ea14ae8f2bad
Michael Fenn <mgfenn157@gmail.com>
2022-11-17 12:28:12 -0500

    updated index

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,20 @@
 // Initialize and add the map
-function initMap() {
-    // The location of UNCC
-    const UNCC = { lat: 35.30910662583227, lng: -80.73357613232336 };
-    // The map, centered at UNCC
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: UNCC,
-      gesturehandling: "greedy",
-        
-    });
-    // The marker, positioned at UNCC
-    const marker = new google.maps.Marker({
-      position: UNCC,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWZlbm4yIiwiYSI6ImNsYWp1cGk0aTAzNnUzbnMwZ3o0bm4xNG8ifQ.-FWDnfl7FidedLkI7qIJiA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mfenn2/clajw6p60001j14qphmp5iz3n', // style URL
+    zoom: 9 // starting zoom
+});
+
+//placeholder values
+var very_high = 1000;
+var high = 800;
+var medium = 600;
+var low = 400;
+var very_low = 200;
+
+map.setPaintProperty("Buildings", "fill-color",
+    ["match", ['get',"id"], 1060166212, "ff0000"]);
+
+
