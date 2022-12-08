221266894b7d766099a2d9b884da195a325729e5
Michael Fenn <mgfenn157@gmail.com>
2022-12-07 20:10:49 -0500

    update dropdown

--- docs/index.js
+++ docs/index.js
@@ -6,9 +6,9 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/mfenn2/clajw6p60001j14qphmp5iz3n', // style URL
 });
 window.onload = function(){
-      let day = document.getElementById('day').value();
-      let hour = document.getElementById('hour').value();
-      let covid = document.getElementById('covid').value();
+      var day = document.getElementById('day').value();
+      var hour = document.getElementById('hour').value();
+      var covid = document.getElementById('covid').value();
       console.log(day);
       console.log(hour);
       console.log(covid);
