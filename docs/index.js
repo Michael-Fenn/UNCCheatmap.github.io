731d434f0857700fd33e08371128e492be42ba1f
Michael Fenn <mgfenn157@gmail.com>
2022-12-08 13:01:11 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -12,7 +12,7 @@ window.onload = function(){
     async function getText(file) {
       let x = await fetch(file);
       let y = await x.text();
-      let arr = y.replace( /\n/g, " " ).split( " " )
+      let arr = y.match(/[^\s]+/g);
       var table = document.getElementById("tableBody");
       for(var j = table.rows.length; j > 0; j--){
         table.deleteRow(j-1);
